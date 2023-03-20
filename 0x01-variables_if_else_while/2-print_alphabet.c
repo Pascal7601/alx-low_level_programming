049e21a7d3a204595a5e14fc4aa1bb3c10df63f4
Pascal7601 <levispascal63@gmail.com>
2023-03-20 15:46:31 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: 'Prints alphabet lowercase'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 97;
+	int c = 65;
+
+	while (a <=122)
+	{
+		putchar(a);
+		a++;
+	}
+	while (c <= 90)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
