4888efc477379e9210215e3e4735a68fd6e7bad7
Pascal7601 <levispascal63@gmail.com>
2024-05-10 12:55:57 -0700

    linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+/**
+ * searches for a value in an array of integers using the 
+ * Linear search algorithm
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+			break;
+		}
+
+	}
+	return (-1);
+}
+
