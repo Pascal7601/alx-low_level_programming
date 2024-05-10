404d0f72fd22c70b956725ebabde412391c646f5
Pascal7601 <levispascal63@gmail.com>
2024-05-10 12:53:26 -0700

    linear search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+/* header files*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+/**
+ * function prototypes
+ */
+int linear_search(int *array, size_t size, int value);
+
+#endif
